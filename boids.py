eb26f1db852b5da69b5a35334b3076da76cf0ebf
EulleSA <eulle100@gmail.com>
2018-11-06 12:13:45 -0300

    Pontos na tela

--- boids.py
+++ boids.py
@@ -1,36 +1,41 @@
 import os,sys
 import pygame
+import numpy as np
+ 
 from pygame.locals import *
+
+BLACK=(0,0,0)
+WHITE = (255, 255, 255)
+
+done=False
 
 try:
     pygame.init()
 except:
     print("O modulo pygame não foi inicializado/instalado corretamente")
 
-while 1:
-
-    screen = pygame.display.set_mode((468,400))
-    pygame.display.set_caption('Boids')
-    pygame.mouse.set_visible(0)
-
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((250,250,250))
-
-    if pygame.font:
-        font = pygame.font.Font(None,36)
-        text = font.render("BOIDS",1, (10,10,10))
-        textpos = text.get_rect(centerx=background.get_width()/2)
-        background.blit(text,textpos)
 
 
-    screen.blit(background,(0,0))
-    pygame.display.flip()
+def create_point(surface,list_pos):
+    for ii in list_pos:
+        print(ii)
+        pygame.draw.circle(surface, BLACK, ii , 5)
 
+
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption('Boids')
+pygame.mouse.set_visible(0)
+
+list_pos = np.random.randint(400, size=(100,2))
+
+screen.fill(WHITE)
+create_point(screen,list_pos)       
+pygame.display.flip()
+
+while not done:
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-        
+            done=True
 
 
     
